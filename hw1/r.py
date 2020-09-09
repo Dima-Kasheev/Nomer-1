4d47e20dc03280f9623a0a28ea6a362bd637bb6d
Dima-Kasheev <52133651+Dima-Kasheev@users.noreply.github.com>
2020-09-09 16:04:13 +0300

    Create r.py

---
+++ hw1/r.py
@@ -0,0 +1,21 @@
+file = open('zash.txt', 'r')
+
+text = "".join(file)
+
+words = 0
+
+pos = 'out'
+
+for i in text:
+
+    if i != ' ' and pos == 'out':
+
+        words += 1
+
+        pos = 'in'
+
+    elif i == ' ':
+
+        pos = 'out'
+
+print('Words: ', words)
